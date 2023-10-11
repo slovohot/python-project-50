701cb08002bde0403948951381337013522db8cd
slovohot <antoshkamc.a@gmail.com>
2023-10-12 01:22:36 +0300

    add gendiff yaml

--- gendiff/logic/generate_diff.py
+++ gendiff/logic/generate_diff.py
@@ -1,20 +1,18 @@
-import json
+import yaml
 
 
 # accept json and returns a dict
 def get_data(file_path):
     with open(file_path, 'r') as file:
-        data = json.load(file)
+        data = yaml.safe_load(file)
         return data
 
 
 def generate_diff(file_path1, file_path2):
     res = []
-
     # returns a dict for futher processing
     data1 = get_data(file_path1)
     data2 = get_data(file_path2)
-
     # saves dict keys
     data1_keys = sorted(data1.keys())
     data2_keys = sorted(data2.keys())
@@ -22,18 +20,24 @@ def generate_diff(file_path1, file_path2):
     for key in data1_keys:
         value1 = data1[key]
         value2 = data2.get(key)
+        # выглядит очень плохо, но я так и не придумал, как сделать это красивее
         if key not in data2:
-            res.append(f'- {key}: {value1}'.lower())
+            res.append(f'- {key}: {value1}'
+                       .translate(str.maketrans('', '', "[]'")).lower())
 
         elif value1 == value2:
-            res.append(f'  {key}: {value1}'.lower())
+            res.append(f'  {key}: {value1}'
+                       .translate(str.maketrans('', '', "[]'")).lower())
 
         else:
-            res.append(f'- {key}: {value1}'.lower())
-            res.append(f'+ {key}: {value2}'.lower())
+            res.append(f'- {key}: {value1}'
+                       .translate(str.maketrans('', '', "[]'")).lower())
+            res.append(f'+ {key}: {value2}'
+                       .translate(str.maketrans('', '', "[]'")).lower())
 
     for key in data2_keys:
         if key not in data1:
-            res.append(f'+ {key}: {data2[key]}'.lower())
+            res.append(f'+ {key}: {data2[key]}'
+                       .translate(str.maketrans('', '', "[]'")).lower())
 
     return '{\n' + '\n'.join(res) + '\n}'
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,15 +1,16 @@
 from gendiff.logic.generate_diff import generate_diff
+from fixtures.fixtures import open_result
 
 
-file_path1 = 'source_file/file1.json'
-file_path2 = 'source_file/file2.json'
+file_json1 = 'source_file/file1.json'
+file_json2 = 'source_file/file2.json'
+file_yaml1 = 'source_file/file1.yaml'
+file_yaml2 = 'source_file/file2.yaml'
 
 
-def open_result():
-    with open('tests/result_json.txt', 'r') as file:
-        content = file.read()
-    return content
-
-
-def test_generate_diff():
-    assert generate_diff(file_path1, file_path2) == open_result()
+def test_generate_diff(open_result):
+    result_json = generate_diff(file_json1, file_json2)
+    result_yaml = generate_diff(file_yaml1, file_yaml2)
+    assert result_json == open_result
+    assert result_yaml == open_result
+    assert result_json == result_yaml
