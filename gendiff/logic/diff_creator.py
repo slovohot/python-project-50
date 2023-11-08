f6d8a1097d1856098977a7126df09189e5e1e654
slovohot <antoshkamc.a@gmail.com>
2023-11-08 14:33:28 +0300

    refactoring #2

--- gendiff/logic/diff_creator.py
+++ gendiff/logic/diff_creator.py
@@ -1,53 +1,46 @@
 
 def diff_creator(old_dict, new_dict):
+    def create_nested_updated(old_value, new_value):
+        if isinstance(old_value, dict) and isinstance(new_value, dict):
+            return {
+                'status': 'nested',
+                'nested': diff_creator(old_value, new_value)
+            }
+        else:
+            return {
+                'status': 'updated',
+                'old_value': old_value,
+                'new_value': new_value
+            }
+
     diff = []
-    keys = sorted(list(set(old_dict.keys()).union(new_dict.keys())))
+    keys = sorted(set(old_dict.keys()).union(new_dict.keys()))
 
     for key in keys:
-        res = {'name': key}
-        res.update(create_edits(old_dict, new_dict, key))
-        diff.append(res)
+        old_value = old_dict.get(key)
+        new_value = new_dict.get(key)
+
+        if old_value == new_value:
+            edits = {
+                'status': 'equal',
+                'old_value': old_value
+            }
+
+        elif key in old_dict and key in new_dict:
+            edits = create_nested_updated(old_value, new_value)
+
+        elif key in old_dict:
+            edits = {
+                'status': 'removed',
+                'old_value': old_value
+            }
+            
+        else:  # key in new_dict
+            edits = {
+                'status': 'added',
+                'new_value': new_value
+            }
+
+        diff.append({'name': key, **edits})
 
     return diff
-
-
-def create_edits(old_dict, new_dict, key):
-    old_value = old_dict.get(key)
-    new_value = new_dict.get(key)
-
-    if old_value == new_value:
-        edits = {
-            'status': 'equal',
-            'old_value': old_value
-        }
-
-    elif key in old_dict and key in new_dict:
-        edits = create_nested_updated(old_value, new_value)
-
-    elif key in old_dict:
-        edits = {
-            'status': 'removed',
-            'old_value': old_value
-        }
-
-    else:  # key in new_dict:
-        edits = {
-            'status': 'added',
-            'new_value': new_value
-        }
-
-    return edits
-
-
-def create_nested_updated(old_value, new_value):
-    if isinstance(old_value, dict) and isinstance(new_value, dict):
-        return {
-            'status': 'nested',
-            'nested': diff_creator(old_value, new_value)
-        }
-    else:
-        return {
-            'status': 'updated',
-            'old_value': old_value,
-            'new_value': new_value
-        }
--- gendiff/logic/file_parser.py
+++ gendiff/logic/file_parser.py
@@ -4,16 +4,19 @@ import os
 
 
 # accept json/yaml and returns a dict
-def get_data(file_path):
-    directory_path = 'tests/fixtures/'
-    _, extension = os.path.splitext(file_path)
-    file_path = os.path.join(directory_path, file_path)
+def get_data(file_path, default_directory='tests/fixtures/'):
+    if os.path.isabs(file_path):
+        full_file_path = file_path
+    else: 
+        full_file_path = os.path.join(default_directory, file_path)
+
+    _, extension = os.path.splitext(full_file_path)
 
     if extension not in ['.json', '.yml', '.yaml']:
         raise Exception(f"Unsupported file format: {extension}. "
                         f"Please use .json, .yml, or .yaml files.")
 
-    with open(file_path, 'r') as file:
+    with open(full_file_path, 'r') as file:
         if extension == '.json':
             return json.load(file)
 
